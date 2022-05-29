1164e192bb96e8d0c444e90676631815be70603e
LeoBrasileo <leobraginski@gmail.com>
2022-05-29 17:00:05 -0300

    plantilla

---
+++ cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/leobr/OneDrive/Desktop/UBA/Algo 1/AyED1-TPS/lib/googletest-master
+# Build directory: C:/Users/leobr/OneDrive/Desktop/UBA/Algo 1/AyED1-TPS/cmake-build-debug/lib/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
