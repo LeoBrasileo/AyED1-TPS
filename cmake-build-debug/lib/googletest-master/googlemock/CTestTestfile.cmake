623087d2736b68d2ac37f0ccc2e9993b67447848
LeoBrasileo <leobraginski@gmail.com>
2022-06-06 14:47:05 -0300

    emprolijando repo

--- cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/clinux01/Escritorio/AyED1-TPS/lib/googletest-master/googlemock
-# Build directory: /home/clinux01/Escritorio/AyED1-TPS/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
